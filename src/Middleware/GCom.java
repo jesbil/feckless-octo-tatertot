4319259e7122037a68f66f211cd43e36f1a903b4
Petter Johansson <oi12pjn@cs.umu.se>
2015-10-08 19:53:24 +0200

    Vill börja på nya modulen lolz

---
+++ src/Middleware/GCom.java
@@ -0,0 +1,9 @@
+package Middleware;
+
+/**
+ * Created by oi12pjn on 2015-10-08.
+ */
+public class GCom {
+
+    //KNYTER IHOP SÄCKEN JÄVLIGT FRÄSCHT
+}
---
+++ src/Middleware/MessageOrdering.java
@@ -0,0 +1,9 @@
+package Middleware;
+
+/**
+ * Created by oi12pjn on 2015-10-08.
+ */
+public class MessageOrdering {
+
+    // NY MODUL REDO FÖR BUS
+}
