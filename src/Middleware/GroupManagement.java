3c8a73731c68fa0e2d55b9f430c85bb96b4ca24f
Petter Johansson <oi12pjn@cs.umu.se>
2015-10-08 19:50:03 +0200

    Vill börja på nya modulen lolz

--- src/Middleware/GroupManagement.java
+++ src/Middleware/GroupManagement.java
@@ -1,5 +1,7 @@
 package Middleware;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.ArrayList;
 
 /**
@@ -8,22 +10,46 @@ import java.util.ArrayList;
 public class GroupManagement {
     private ArrayList<Member> members;
     private ArrayList<Group> groups;
+    private Member LocalMember;
 
 
-    public GroupManagement(){
+    public GroupManagement() throws UnknownHostException {
         groups = new ArrayList<Group>();
         members = new ArrayList<Member>();
+        LocalMember = new Member(InetAddress.getLocalHost().getHostAddress());
     }
 
     public void createGroup(String name) {
+        groups.add(new Group(name));
+
+        //SKICKA VIDARE TILL MESSAGE ORDERING
+    }
+
+    public void joinGroup(String name) throws UnknownHostException {
+        for (int i = 0; i <groups.size() ; i++) {
+            if (groups.get(i).getName().equals(name)){
+                groups.get(i).addMemberToGroup(LocalMember);
+            }
+        }
+
+        //SKICKA VIDARE TILL MESSAGE ORDERING
+    }
+
+    public void leaveGroup(String name) {
+        for (int i = 0; i <groups.size() ; i++) {
+            if (groups.get(i).getName().equals(name)){
+                groups.get(i).removeMemberFromGroup(LocalMember);
+            }
+        }
+
+        //SKICKA VIDARE
+    }
+
+    public void sendMessage(String msg){
+
+
 
     }
 
-    public void joinGroup(String name) {
 
-    }
-
-    public void leaveGroup() {
-
-    }
 }
