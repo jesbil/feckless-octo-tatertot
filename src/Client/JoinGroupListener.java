b9dcba2888808606b32cc654f9263c84fbf9dd96
Jesper Bilander <c12jbr@cs.umu.se>
2015-10-11 16:48:35 +0200

    d

--- src/Client/JoinGroupListener.java
+++ src/Client/JoinGroupListener.java
@@ -5,6 +5,9 @@ import Middleware.GCom;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.net.UnknownHostException;
+import java.rmi.NotBoundException;
+import java.rmi.RemoteException;
 
 /**
  * Created by c12jbr on 2015-10-11.
@@ -16,6 +19,14 @@ public class JoinGroupListener implements ActionListener {
         while (groupName==null){
             groupName = JOptionPane.showInputDialog(null, "What group do you want to join", "balle");
         }
-        GCom.joinGroup(groupName);
+        try {
+            GCom.joinGroup(groupName);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (RemoteException e) {
+            e.printStackTrace();
+        } catch (NotBoundException e) {
+            e.printStackTrace();
+        }
     }
 }
