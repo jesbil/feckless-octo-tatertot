3b6c9a3767aaad2384e0a42b756ebb58d73c14fc
Jesper Bilander <c12jbr@cs.umu.se>
2015-10-14 07:51:30 +0200

    invalid order?

--- src/Middleware/VectorClock.java
+++ src/Middleware/VectorClock.java
@@ -80,7 +80,7 @@ public class VectorClock implements Serializable{
         }
         if(this.biggerThen(vc,sender)){
             System.out.println("biggerthen");
-            return CLOCK_TYPE_BT;
+            return CLOCK_TYPE_EQ;
         }
         System.out.println("concurrent");
         return CLOCK_TYPE_CONC;
